99515d9c4c04689b70be627188b76b04bce0103f
Purna Shrestha <crush9918@gmail.com>
2025-01-29 13:47:14 +0545

    feat: Add create account, login, see notes, and edit notes feature

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
+require("dotenv").config();
+
+const config = require("./config");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
+const User = require("./models/user.model");
+const Note = require("./models/note.model");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const jwt = require("jsonwebtoken");
+const { autthenticateToken } = require("./utilities");
 
 app.use(express.json());
 
@@ -15,8 +28,186 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Create an account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full name is required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const isUser = await User.findOne({ email });
+
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: "Email is already in use",
+    });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Account created successfully",
+  });
+});
+
+// Login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const userInfo = await User.findOne({
+    email,
+    password,
+  });
+
+  if (!userInfo) {
+    return res.status(400).json({ error: true, message: "User not found." });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+
+    return res.json({
+      error: false,
+      user: userInfo,
+      accessToken,
+      message: "Login successful",
+    });
+  } else {
+    return res
+      .status(400)
+      .json({ error: true, message: "Invalid credentials" });
+  }
+});
+
+// Get all notes
+app.post("/add-note", autthenticateToken, async (req, res) => {
+  const { title, content, tags } = req.body;
+  const { user } = req.user;
+
+  if (!title) {
+    return res.status(400).json({ error: true, message: "Title is required" });
+  }
+
+  if (!content) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Content is required" });
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id,
+    });
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note added successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal Server Error" });
+  }
+});
+
+// Edit a note
+
+app.put("/edit-note/:noteId", autthenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { title, content, tags, isPinned } = req.body;
+  const { user } = req.user;
+
+  if (!title && !content && !tags) {
+    return res
+      .status(400)
+      .json({ error: true, message: "No changes provided." });
+  }
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found." });
+    }
+
+    if (title) {
+      note.title = title;
+    }
+
+    if (content) {
+      note.content = content;
+    }
+
+    if (tags) {
+      note.tags = tags;
+    }
+
+    if (isPinned) {
+      note.isPinned = isPinned;
+    }
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note edited successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal Server Error" });
+  }
+});
+
 app.listen(8000, () => {
-  console.log("Server is running on port 3001");
+  console.log("Server is running on port 8000");
 });
 
 module.exports = app;
